06325637adb235d745a08af30e1d49b395843203
Mygaru Sync <sync@mygaru.com>
2025-12-01 12:22:06 +0000

    Sync

--- pkg/mtls/mtls_server.go
+++ pkg/mtls/mtls_server.go
@@ -3,14 +3,13 @@ package mtls
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"errors"
 	"flag"
 	"fmt"
 	"github.com/mygaru/id-check/pkg/proxy"
 	"github.com/valyala/fasthttp"
+	"github.com/valyala/fastjson"
 	"log"
 	"net"
-	"sync/atomic"
 	"time"
 )
 
@@ -23,13 +22,7 @@ var (
 	mtlsServerCertPath       = flag.String("mtlsServerCertPath", "", "Path to file containing server certificate")
 	mtlsServerPrivateKeyPath = flag.String("mtlsServerPrivateKeyPath", "", "Path to file containing private key corresponding to server certificate")
 	mtlsServerMaxBodySize    = flag.Int("mtlsServerMaxBodySize", 536870912, "Max request body size")
-	mtlsCrlCheckInterval     = flag.Duration("mtlsCrlCheckInterval", 10*time.Second, "How often to refresh myGaru CRL")
-)
-
-var (
-	// contains Revocation List
-	crlAtomic    atomic.Value
-	crlLastCheck atomic.Value
+	mtlsReputationUrl        = flag.String("mtlsReputationUrl", "https://ca.mygaru.com/reputation", "Where to check cert status")
 )
 
 func RunServer(handler fasthttp.RequestHandler) {
@@ -49,18 +42,25 @@ func RunServer(handler fasthttp.RequestHandler) {
 		ClientAuth:   tls.RequireAndVerifyClientCert,
 		VerifyPeerCertificate: func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
 			cert := verifiedChains[0][0]
-			issuerCert := verifiedChains[0][len(verifiedChains[0])-1]
-
 			log.Printf("Validating server certificate with CRL (serial: %s)", cert.SerialNumber.String())
 
-			err := queryCRL(cert, issuerCert)
+			status, reason, err := CheckCertReputation(cert)
 			if err != nil {
-				log.Printf("CRL query failed: %s", err)
-				return err
+				return fmt.Errorf("error checking reputation: %s", err)
 			}
 
-			log.Printf("Certificate CRL check successful.")
-			return nil
+			log.Printf("Cert %s has status=%s reason=%s", cert.SerialNumber.String(), status, reason)
+
+			switch status {
+			case CertStatusRevoked:
+				return fmt.Errorf("revoked certificate: %s", reason)
+			case CertStatusGood:
+				return nil
+			case CertStatusUnknown:
+				return fmt.Errorf("certificate with unkown status")
+			default:
+				return fmt.Errorf("unkown status type: %s", status)
+			}
 		},
 	}
 
@@ -81,87 +81,55 @@ func RunServer(handler fasthttp.RequestHandler) {
 	}
 }
 
-func setCRL(crlURL string) error {
+const (
+	CertStatusRevoked = "revoked"
+	CertStatusGood    = "good"
+	CertStatusUnknown = "unknown"
+)
+
+// CheckCertReputation checks reputation of cert, and returns status, reason, and error
+func CheckCertReputation(cert *x509.Certificate) (string, string, error) {
 	req := fasthttp.AcquireRequest()
 	resp := fasthttp.AcquireResponse()
-
-	req.SetRequestURI(crlURL)
-	req.Header.SetMethod(fasthttp.MethodGet)
 
 	defer func() {
 		fasthttp.ReleaseResponse(resp)
 		fasthttp.ReleaseRequest(req)
 	}()
 
-	client, err := proxy.GetClient(req, crlURL)
+	url := *mtlsReputationUrl + "/" + cert.SerialNumber.String()
+
+	log.Printf("Checking reputation of %s", url)
+
+	client, err := proxy.GetClient(req, url)
 	if err != nil {
-		return fmt.Errorf("failed to get proxy client: %w", err)
+		return "", "", fmt.Errorf("failed to get proxy client: %w", err)
 	}
 
-	log.Printf("\n\n REQUEST: %s \n\n", req.String())
+	req.SetRequestURI(url)
+	req.Header.SetMethod(fasthttp.MethodGet)
 
 	err = client.DoTimeout(req, resp, 10*time.Second)
 	if err != nil {
-		return err
+		return "", "", err
 	}
 
 	if resp.StatusCode() != fasthttp.StatusOK {
-		return fmt.Errorf("got code %d when trying to get CRL from %s: %s", resp.StatusCode(), crlURL, string(resp.Body()))
+		return "", "", fmt.Errorf("wanted 200 OK, got %d: %s", resp.StatusCode(), resp.Body())
 	}
 
-	crl, err := x509.ParseRevocationList(resp.Body())
+	v, err := fastjson.ParseBytes(resp.Body())
 	if err != nil {
-		return err
+		return "", "", fmt.Errorf("failed to parse bytes: %w", err)
 	}
 
-	crlAtomic.Store(crl)
-	crlLastCheck.Store(time.Now())
-	return nil
-}
+	v = v.Get("0")
 
-func queryCRL(cert *x509.Certificate, issuerCert *x509.Certificate) error {
-	if crlAtomic.Load() == nil || time.Now().Sub(crlLastCheck.Load().(time.Time)) >= *mtlsCrlCheckInterval {
-
-		if len(cert.CRLDistributionPoints) != 1 {
-			return fmt.Errorf("expected 1 distribution point in issuer certificate, got: %v", issuerCert.CRLDistributionPoints)
-		}
-
-		log.Printf("[*] Trying to renew CRL from %q. Len URL = %d", cert.CRLDistributionPoints[0], len(cert.CRLDistributionPoints[0]))
-
-		err := setCRL(cert.CRLDistributionPoints[0])
-		if err != nil {
-			return fmt.Errorf("failed to renew CRL: %w", err)
-		}
+	status := v.GetStringBytes("status")
+	if len(status) == 0 {
+		return "", "", fmt.Errorf("no status in json body: %s", resp.Body())
 	}
 
-	crl, ok := crlAtomic.Load().(*x509.RevocationList)
-	if !ok {
-		return errors.New("CRL Atomic Load failed")
-	}
+	return string(status), string(v.GetStringBytes("reason")), nil
 
-	log.Printf("[*] Checking CRL signature.")
-	log.Printf("Issuer CN: %s", issuerCert.Subject.CommonName)
-	log.Printf("CRL Issuer: %s", crl.Issuer.String())
-
-	err := crl.CheckSignatureFrom(issuerCert)
-	if err != nil {
-		return err
-	}
-
-	log.Printf("[*] Checking CRL validity.")
-	if crl.NextUpdate.Before(time.Now()) {
-		return fmt.Errorf("CRL is outdated")
-	}
-
-	log.Printf("[*] Searching for our certificate...")
-	for _, revokedCertificate := range crl.RevokedCertificateEntries {
-		log.Printf("[*] Revoked certificate serial: %s.", revokedCertificate.SerialNumber.String())
-		if revokedCertificate.SerialNumber.Cmp(cert.SerialNumber) == 0 {
-			log.Printf("[-] Found validated certificate in list of revoked ones.")
-			return fmt.Errorf("certificate was revoked")
-		}
-	}
-
-	log.Printf("[+] Did not find validated certificate among revoked ones.")
-	return nil
 }
